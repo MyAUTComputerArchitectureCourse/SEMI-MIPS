0a794d4092722e549ec0ef0f21ddef072f1b3baa
Ahmad Anvari <ahmad.anv@gmail.com>
2017-06-08 13:27:02 +0430

    Problem fix for fixed size multipliction component

--- src/mips/datapath/alu/components/multiplication_component.vhd
+++ src/mips/datapath/alu/components/multiplication_component.vhd
@@ -12,55 +12,55 @@ use IEEE.std_logic_1164.all;
 
 entity MULTIPLICATION_COMPONENT is
 	port(
-		INPUT1		: in std_logic_vector(16/2 - 1 downto 0);
-		INPUT2		: in std_logic_vector(16/2 - 1 downto 0);
-		OUTPUT		: out std_logic_vector(16 - 1 downto 0)
+		INPUT1		: in std_logic_vector(7 downto 0);
+		INPUT2		: in std_logic_vector(7 downto 0);
+		OUTPUT		: out std_logic_vector(7 downto 0)
 	
 	);
 end entity;
 
 architecture MULTIPLICATION_COMPONENT_ARCH of MULTIPLICATION_COMPONENT is
-	component ADDER_SUBTRACTOR_COMPONENT is
+	component ADDER_SUBTRACTOR_COMPONENT_MULTIPLIER is
 		port(
 			CARRY_IN	: in  std_logic;
-			INPUT1		: in  std_logic_vector(16 - 1 downto 0);
-			INPUT2		: in  std_logic_vector(16 - 1 downto 0);
+			INPUT1		: in  std_logic_vector(7 downto 0);
+			INPUT2		: in  std_logic_vector(7 downto 0);
 			IS_SUB		: in  std_logic;											-- 0 for add and 1 for subtraction
-			SUM			: out std_logic_vector(16 - 1 downto 0);
+			SUM			: out std_logic_vector(7 downto 0);
 			CARRY		: out std_logic
 	      );
 	end component;
 	
-	type arraySignals is array (0 to 16/2 - 1) of std_logic;
-	type arr1 is array (0 to 16/2 - 1) of std_logic_vector(16/2 - 1 downto 0);
+	type arraySignals is array (0 to 7) of std_logic;
+	type arr1 is array (0 to 7) of std_logic_vector(7 downto 0);
 	signal cables 	: arraySignals;
 	signal khar, gav, ain, summ : arr1;
 
 	
 begin
 	MAKING_IN:
-	for W in 0 to 16/2 - 1 generate
+	for W in 0 to 7 generate
 		ANDING:
-		for J in 0 to 16/2 - 1 generate
+		for J in 0 to 7 generate
 			gav(W)(j) <= INPUT1(J) and INPUT2(W);
 		end generate;
 	end generate;
 
-	ain(0)(16/2 - 1) <= '0';
+	ain(0)(7) <= '0';
 
 	KHAAR:
-		for L in 0 to 16/2 - 2 generate
+		for L in 0 to 7 generate
 			ain(0)(L) <= gav(0)(L + 1);
 		end generate;
 		output(0) <= gav(0)(0);
 	CONNECT:
-	for I in 0 to 16/2 - 2 generate
-		MODULE: ADDER_SUBTRACTOR_COMPONENT
+	for I in 0 to 7 generate
+		MODULE: ADDER_SUBTRACTOR_COMPONENT_MULTIPLIER
 		port map('0',gav(I + 1) ,ain(I) , '0', summ(I), cables(I));
 
-		ain(I + 1)(16/2 - 1) <= cables(I);
+		ain(I + 1)(7) <= cables(I);
 		MAKING:
-		for K in 0 to 16/2 - 2 generate
+		for K in 0 to 7 generate
 			ain(I + 1)(K) <= summ(I)(K + 1);
 		end generate;
 
@@ -68,12 +68,12 @@ begin
 	end generate;
 	
 	-- AH:
-	-- for Q in 0 to 16/2 - 1 generate
-	-- 	output(Q + 16/2) <= summ(16/2 - 2)(Q);
+	-- for Q in 0 to 8 - 1 generate
+	-- 	output(Q + 8) <= summ(7)(Q);
 	-- end generate;
 	AH:
-	for Q in 0 to 16/2 - 2 generate
-		output(Q + 16/2) <= summ(16/2 - 2)(Q + 1);
+	for Q in 0 to 7 generate
+		output(Q + 8) <= summ(7)(Q + 1);
 	end generate;
-	output(16 - 1) <= cables(16/2 - 2);
+	output(16 - 1) <= cables(7);
 end architecture;
