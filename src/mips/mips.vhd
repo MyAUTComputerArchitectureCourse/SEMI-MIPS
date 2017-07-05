49aa3bed6524ba1749a7afdc717fc0f102f44c38
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-06 00:35:14 +0430

    Adding component

--- src/mips/mips.vhd
+++ src/mips/mips.vhd
@@ -9,6 +9,51 @@ entity SEMI_MIPS is
 end entity;
 
 architecture ALU_ARCH of ALU is
-begin
-end architecture;
+	component ALU is
+		port(
+			CARRY_IN	: in  std_logic;
+			INPUT1		: in  std_logic_vector(16 - 1 downto 0);
+			INPUT2		: in  std_logic_vector(16 - 1 downto 0);
+			OPERATION	: in  std_logic_vector(3 downto 0);
+			OUTPUT		: out std_logic_vector(16 - 1 downto 0);
+			CARRY_OUT	: out std_logic;
+			ZERO_OUT	: out std_logic
+	      );
+	end component;
+	component STATUS_REGISTER is
+		PORT (
+	        carryIn, overflowIn : IN std_logic;
+	        data : IN std_logic_vector (15 DOWNTO 0);
+	        carry, zero, sign, parity, borrow, overflow : OUT std_logic
+	    );
+	end component;
+	component registerFile is
+		port (
+			CLK		:	in	std_logic;
+			W_EN	:	in	std_logic;
+			INPUT	:	in	std_logic_vector(15 downto 0);
+			IN_ADR	:	in	std_logic_vector(3 downto 0);
+			OUT1_ADR:	in	std_logic_vector(3 downto 0);
+			OUT2_ADR:	in	std_logic_vector(3 downto 0);
+			OUTPUT1	:	out	std_logic_vector(15 downto 0);
+			OUTPUT2	:	out	std_logic_vector(15 downto 0)
+		);
+	end component;
+	component ADDRESS_UNIT is
+		PORT (
+	        Iside : IN std_logic_vector (7 DOWNTO 0);
+	        Address : OUT std_logic_vector (7 DOWNTO 0);
+	        clk, ResetPC, I, PCplus1 : IN std_logic;
+	        EnablePC : IN std_logic
+	    );
+	end component;
 	
+	component reg16b is
+		port(clk, load, reset : in STD_LOGIC;
+			input : in STD_LOGIC_VECTOR (15 downto 0);
+			output : out STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000"
+		);
+	end component;
+begin
+	
+end architecture;
