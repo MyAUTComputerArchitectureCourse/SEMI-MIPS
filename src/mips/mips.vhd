60a45bc475aee8096ca5a4fc97d5190291698536
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-06 00:09:21 +0430

    Structure of data path

--- src/mips/mips.vhd
+++ src/mips/mips.vhd
@@ -1,1 +1,14 @@
+entity SEMI_MIPS is
+  port (
+    clk   			: in  std_logic;
+    we      		: out  std_logic;
+    address 		: out  std_logic_vector(7 downto 0);
+    memory_in		: in  std_logic_vector(15 downto 0);
+    memory_out		: out  std_logic_vector(15 downto 0)
+  );
+end entity;
 
+architecture ALU_ARCH of ALU is
+begin
+end architecture;
+	
